80e7639c0e57da8605bb2df5d1c6ab240a8f3773
Paul Nation <nonhermitian@gmail.com>
2022-11-03 16:39:52 -0400

    updates

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools.command.develop import develop
 from setuptools.command.install import install
 
 MAJOR = 0
-MINOR = 5
+MINOR = 6
 MICRO = 0
 
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
